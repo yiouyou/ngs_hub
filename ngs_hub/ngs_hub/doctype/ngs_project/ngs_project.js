cde29d1f3e32a0ecc3d04120b822ed8df77737dc
Juan Navarro [Huawei] <jint2376@gmail.com>
2026-04-30 11:10:39 +0800

    creates the button, but now no redirection

--- ngs_hub/ngs_hub/doctype/ngs_project/ngs_project.js
+++ ngs_hub/ngs_hub/doctype/ngs_project/ngs_project.js
@@ -6,25 +6,31 @@
 
 // 	},
 // });
-frappe.ui.form.on('NGS Project', {
-  refresh(frm) {
-    frm.add_custom_button(__('→ Customer'), () => {
-      if (frm.doc.customer) {
-        frappe.set_route('Form', 'NGS Customer', frm.doc.customer);
-      } else {
-        frappe.msgprint(__('No associated Customer found.'));
-      }
-    });
-    if (frm.doc.class_type === 'RNAseq') {
-      frappe.set_route('Form', 'NGS Pipeline Run', docname);
-    }
-    frm.add_custom_button(__('+ Sample Transfer'), () => {
-      frappe.new_doc('NGS Sample Transfer', {}, (doc) => {
-        doc.customer = frm.doc.customer
-        doc.class_type = frm.doc.class_type;
-        doc.project = frm.doc.project_id;
-        doc.save();
-      });
-    });
-  }
+frappe.ui.form.on("NGS Project", {
+	refresh(frm) {
+		frm.add_custom_button(__("→ Customer"), () => {
+			if (frm.doc.customer) {
+				frappe.set_route("Form", "NGS Customer", frm.doc.customer);
+			} else {
+				frappe.msgprint(__("No associated Customer found."));
+			}
+		});
+		if (frm.doc.class_type === "RNAseq") {
+			frm.add_custom_button(__("Sequencing"), () => {
+				frappe.set_route("Form", "NGS Pipeline Run", docname);
+			});
+		}
+
+		if (frm.doc.class_type === "RNAseq") {
+			frappe.set_route("Form", "NGS Pipeline Run", docname);
+		}
+		frm.add_custom_button(__("+ Sample Transfer"), () => {
+			frappe.new_doc("NGS Sample Transfer", {}, (doc) => {
+				doc.customer = frm.doc.customer;
+				doc.class_type = frm.doc.class_type;
+				doc.project = frm.doc.project_id;
+				doc.save();
+			});
+		});
+	},
 });
