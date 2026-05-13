e6493ebb8205bc5c9a316271713fda059d2eea54
Juan Navarro [Huawei] <jint2376@gmail.com>
2026-05-13 17:23:40 +0800

    running console log to check the output

--- ngs_hub/ngs_hub/doctype/ngs_pipeline_run/ngs_pipeline_run.js
+++ ngs_hub/ngs_hub/doctype/ngs_pipeline_run/ngs_pipeline_run.js
@@ -8,32 +8,36 @@ frappe.ui.form.on("NGS Pipeline Run", {
 	},
 	refresh(frm) {
 		frm.add_custom_button(__("Validate"), () => {
-			frappe.call({
-				method: "ngs_hub.api.pipeline.validate_run",
-				args: { doc: frm.doc },
-				freeze: true,
-				callback: (r) => {
-					if (!r.exc) {
-						frappe.msgprint(
-							__("Validate result: {0}", [r.message.command]),
-						);
-					}
-				},
-			});
+			const payload = buildPayload(frm);
+			console.log("Run payload:", payload);
+			// frappe.call({
+			// 	method: "ngs_hub.api.pipeline.validate_run",
+			// 	args: { request_data: payload },
+			// 	freeze: true,
+			// 	callback: (r) => {
+			// 		if (!r.exc) {
+			// 			frappe.msgprint(
+			// 				__("Validate result: {0}", [r.message.command]),
+			// 			);
+			// 		}
+			// 	},
+			// });
 		});
 		frm.add_custom_button(__("Run"), () => {
-			frappe.call({
-				method: "ngs_hub.api.pipeline.run",
-				args: { doc: frm.doc },
-				freeze: true,
-				callback: (r) => {
-					if (!r.exc) {
-						frappe.msgprint(
-							__("Workflow queued: {0}", [r.message.workflow_id]),
-						);
-					}
-				},
-			});
+			const payload = buildPayload(frm);
+			console.log("Run payload:", payload);
+			// frappe.call({
+			// 	method: "ngs_hub.api.pipeline.run",
+			// 	args: { request_data: payload },
+			// 	freeze: true,
+			// 	callback: (r) => {
+			// 		if (!r.exc) {
+			// 			frappe.msgprint(
+			// 				__("Workflow queued: {0}", [r.message.workflow_id]),
+			// 			);
+			// 		}
+			// 	},
+			// });
 		});
 		frm.refresh_field("existing_attachment");
 		frm.refresh_field("upload_csv");
@@ -48,3 +52,45 @@ frappe.ui.form.on("NGS Pipeline Run", {
 		frm.refresh();
 	},
 });
+
+/**
+ * Gather all the bits from frm.doc and return a WorkflowRequest‐shaped dict
+ */
+function buildPayload(frm) {
+	// example: grab S3 creds off the form
+	const s3_credentials = {
+		role_arn: frm.doc.role_arn,
+		access_key_id: frm.doc.access_key_id,
+		secret_access_key: frm.doc.secret_access_key,
+		session_token: frm.doc.session_token,
+	};
+
+	// build input buckets (you may have one or many)
+	const s3_input_config = (frm.doc.s3_input_paths || []).map((p) => ({
+		bucket: p.bucket_name,
+		region: p.bucket_region,
+		prefix: p.prefix,
+		save_dir_name: p.save_to_folder,
+	}));
+
+	// assume a single output bucket section in the form
+	const s3_output_config = {
+		bucket: frm.doc.output_bucket_name,
+		region: frm.doc.output_bucket_region,
+	};
+
+	// pipeline config
+	const pipeline_config = {
+		pipeline_type: frm.doc.pipeline_type,
+		sample_id: frm.doc.sample_id,
+		params: JSON.parse(frm.doc.params_json || "{}"),
+		time_limit_hours: frm.doc.time_limit_hours,
+	};
+
+	return {
+		s3_credentials,
+		s3_input_config,
+		s3_output_config,
+		pipeline_config,
+	};
+}
