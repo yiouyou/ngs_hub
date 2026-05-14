64f389f59e6cf6cb57b1564f29900fb3d3e50f08
Juan Navarro [Huawei] <jint2376@gmail.com>
2026-05-14 17:17:14 +0800

    fixed bug on the map

--- ngs_hub/ngs_hub/doctype/ngs_pipeline_run/ngs_pipeline_run.js
+++ ngs_hub/ngs_hub/doctype/ngs_pipeline_run/ngs_pipeline_run.js
@@ -123,7 +123,7 @@ async function buildPayload(frm) {
 
 	// pipeline config
 	const pipeline_config = {
-		pipeline_type: pipeline_types[frm.doc.class_type],
+		pipeline_type: pipeline_types.get(frm.doc.class_type),
 		sample_id: frm.doc.sample_id,
 		params: params,
 	};
